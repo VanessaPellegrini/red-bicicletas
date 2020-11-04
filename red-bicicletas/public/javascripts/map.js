2278aae92ec1bf80974ab665f5efb03ea9957084
VanessaPellegrini <vanessapellegrini@hotmail.com>
2020-11-03 21:39:11 -0300

    api

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -5,6 +5,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 }).addTo(map);
 
-L.marker([-34.6012424, -58.3861497]).addTo(map);
-L.marker([-34.596932, -58.3881287]).addTo(map);
-L.marker([-34.599564, -58.3778777]).addTo(map);
+//L.marker([-34.6012424, -58.3861497]).addTo(map);
+//L.marker([-34.596932, -58.3881287]).addTo(map);
+//L.marker([-34.599564, -58.3778777]).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
